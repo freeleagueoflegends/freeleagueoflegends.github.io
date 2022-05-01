85ddb9ffa53905773088d3229c53e2ff3877090f
freeleagueoflegends <104707042+freeleagueoflegends@users.noreply.github.com>
2022-05-01 07:30:01 +0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAgRY8kp3JwPVjdK0Hiehm2JCSObjkeFVk",
+  authDomain: "computercontrol-be26d.firebaseapp.com",
+  databaseURL: "https://computercontrol-be26d-default-rtdb.firebaseio.com",
+  projectId: "computercontrol-be26d",
+  storageBucket: "computercontrol-be26d.appspot.com",
+  messagingSenderId: "821186447132",
+  appId: "1:821186447132:web:d3bbd14eed4d91264d9cd9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
