fed401cde0fa6bff8f1690e5cc7cc8ea701080c1
freeleagueoflegends <104707042+freeleagueoflegends@users.noreply.github.com>
2022-05-01 07:46:51 +0300

    Update fireConfig.js

--- fireConfig.js
+++ fireConfig.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAgRY8kp3JwPVjdK0Hiehm2JCSObjkeFVk",
-  authDomain: "computercontrol-be26d.firebaseapp.com",
-  databaseURL: "https://computercontrol-be26d-default-rtdb.firebaseio.com",
-  projectId: "computercontrol-be26d",
-  storageBucket: "computercontrol-be26d.appspot.com",
-  messagingSenderId: "821186447132",
-  appId: "1:821186447132:web:d3bbd14eed4d91264d9cd9"
+  apiKey: "AIzaSyB978ZPIOXUr1P7ncW3LtwsbSsYKLOjvHE",
+  authDomain: "lolhack-6d5c9.firebaseapp.com",
+  databaseURL: "https://lolhack-6d5c9-default-rtdb.firebaseio.com",
+  projectId: "lolhack-6d5c9",
+  storageBucket: "lolhack-6d5c9.appspot.com",
+  messagingSenderId: "426906212136",
+  appId: "1:426906212136:web:dc077bac8a199d70e2221d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
